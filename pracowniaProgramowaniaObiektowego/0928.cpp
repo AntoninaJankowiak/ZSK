7b6cc97ae02c56a635c82f14d9eaa772a5be55dd
AntoninaJankowiak <90239369+AntoninaJankowiak@users.noreply.github.com>
2021-09-28 08:41:06 +0200

    Rename Untitled1.cpp to 0928.cpp

--- pracowniaProgramowaniaObiektowego/0928.cpp
+++ pracowniaProgramowaniaObiektowego/0928.cpp
@@ -19,7 +19,7 @@ int main(){
 	setlocale(LC_CTYPE, "polish");
 	Student kowalski{"Janusz", "Nowak", 100,{28,9,2021},{5,4,2,3,4}};
 	
-	cout<<"Imiê i nazwisko: "<<kowalski.name<<" "<<kowalski.surname
+	cout<<"ImiÃª i nazwisko: "<<kowalski.name<<" "<<kowalski.surname
 	<<"\nIdentyfikator: "<<kowalski.id<<"\nData urodzenia: "<<kowalski.dateBirthday.dd<<"-"<<kowalski.dateBirthday.mm
 	<<"-"<<kowalski.dateBirthday.rrrr;
 	return 0;
